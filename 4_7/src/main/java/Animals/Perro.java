89395b9f6198506bcff7e74acf5422bee062c0e5
Mateo Cordero <143677758+Mcordero21@users.noreply.github.com>
2023-10-16 07:46:29 -0500

    Add files via upload

---
+++ 4_7/src/main/java/Animals/Perro.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Animals;
+
+/**
+ *
+ * @author Matte
+ */
+public class Perro extends Canido {
+    public String getSonido() {
+    return "Ladrido";
+    }
+    
+    public String getAlimentos() {
+    return "Carnívoro";
+    }
+    
+    public String getHabitat() {
+    return "Doméstico";
+    }
+    
+    public String getNombreCientifico() {
+    return "Canis lupus familiaris";
+    }
+}
